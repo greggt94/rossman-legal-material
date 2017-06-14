1e95aa813f162f59b16ef35562d85edf830798c6
greggt94 <gtjunior94@gmail.com>
2017-06-13 20:20:02 -0400

    maintenance

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,34 +7,4 @@ $(document).ready(function() {
         draggable: true
     });
 
-    // Submit form functionality
-    $('#submit-button').click(function() {
-        event.preventDefault();
-
-        var data = {
-            firstName: $('#first_name').val(),
-            lastName: $('#last_name').val(),
-            email: $('#email').val(),
-            message: $('#textarea').val()
-        };
-
-        $.ajax({
-                url: 'email.php',
-                type: 'POST',
-                data: data,
-            })
-            .done(function() {
-                console.log("success");
-                $('#first_name').val("");
-                $('#last_name').val("");
-                $('#email').val("");
-                $('#textarea').val("");
-            })
-            .fail(function() {
-                console.log("error");
-
-            });
-
-    });
-
 });
